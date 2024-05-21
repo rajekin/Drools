0fb764df780df10e9bf7ab8b2a79ce48ee2292d0
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2024-05-20 20:57:24 -0400

    Update DateToEpochExample.java

--- DateToEpochExample.java
+++ DateToEpochExample.java
@@ -1,12 +1,33 @@
- public static long getHoursDifference(Date passedInDate) {
-        // Get the current date/time
-        Date currentDate = new Date();
-        
-        // Calculate the difference in milliseconds
-        long millisecondsDifference = currentDate.getTime() - passedInDate.getTime();
-        
-        // Convert milliseconds to hours
-        long hoursDifference = TimeUnit.MILLISECONDS.toHours(millisecondsDifference);
-        
-        return hoursDifference;
+package com.tlp.rating;
+
+public enum ConstructionType {
+    BRICK("BRICK"),
+    STICKS("STICKS"),
+    STRAW("STRAW");
+
+    private final String code;
+
+    ConstructionType(String code) {
+        this.code = code;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Get enum instance by code, ignoring case.
+     *
+     * @param input The input code to search for.
+     * @return The matching enum instance or null if none found.
+     */
+    public static ConstructionType getByCode(String input) {
+        for (ConstructionType type : ConstructionType.values()) {
+            if (type.code.equalsIgnoreCase(input)) {
+                return type;
+            }
+        }
+        return null; // No match found
+    }
+}
+
