7dd2dc66b895309af195f6dc0aa3eda01adacf23
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2024-07-09 20:56:20 -0400

    Update DateToEpochExample.java

--- DateToEpochExample.java
+++ DateToEpochExample.java
@@ -1,35 +1,36 @@
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ProductService {
 
     public void filterAndUpdateProducts(List<Product> products, String type, String name, String fundingSource, BigDecimal newMaxAmount, BigDecimal newMinAmount) {
         for (Product product : products) {
             if (product.getType().equals(type) && product.getName().equals(name)) {
-                List<EligFundingAccount> updatedFundingAccounts = product.getEligFundingAccounts().stream()
-                        .filter(account -> account.getFundingSource().equals(fundingSource))
-                        .peek(account -> {
-                            account.setMaxAmount(newMaxAmount);
-                            account.setMinAmount(newMinAmount);
-                        })
-                        .collect(Collectors.toList());
-
-                product.setEligFundingAccounts(updatedFundingAccounts);
+                for (EligFundingAccount account : product.getEligFundingAccounts()) {
+                    if (account.getFundingSource().equals(fundingSource)) {
+                        account.setMaxAmount(newMaxAmount);
+                        account.setMinAmount(newMinAmount);
+                    }
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         // Sample data for testing
-        List<EligFundingAccount> fundingAccounts = List.of(
+        List<EligFundingAccount> fundingAccounts1 = List.of(
             new EligFundingAccount("source1", new BigDecimal("1000"), new BigDecimal("500")),
             new EligFundingAccount("source2", new BigDecimal("2000"), new BigDecimal("1000"))
         );
 
+        List<EligFundingAccount> fundingAccounts2 = List.of(
+            new EligFundingAccount("source3", new BigDecimal("3000"), new BigDecimal("1500")),
+            new EligFundingAccount("source4", new BigDecimal("4000"), new BigDecimal("2000"))
+        );
+
         List<Product> products = List.of(
-            new Product(1, "type1", "name1", fundingAccounts),
-            new Product(2, "type2", "name2", fundingAccounts)
+            new Product(1, "type1", "name1", fundingAccounts1),
+            new Product(2, "type2", "name2", fundingAccounts2)
         );
 
         ProductService productService = new ProductService();
