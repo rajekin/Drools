cc1c9ea2f85191e87cb4f29bafa5e2875ff1731a
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2024-07-10 02:54:33 -0400

    Update DateToEpochExample.java

--- DateToEpochExample.java
+++ DateToEpochExample.java
@@ -1,14 +1,3 @@
-public List<Product> removeProductByIdAndName(List<Product> products, int id, String name) {
-        List<Product> removedProducts = new ArrayList<>();
-        Iterator<Product> iterator = products.iterator();
-        
-        while (iterator.hasNext()) {
-            Product product = iterator.next();
-            if (product.getId() == id && product.getName().equals(name)) {
-                removedProducts.add(product);
-                iterator.remove();
-            }
-        }
-
-        return removedProducts;
-    }
+products.stream()
+                .filter(product -> !(product.getId() == id && product.getName().equals(name)))
+                .collect(Collectors.toList());
