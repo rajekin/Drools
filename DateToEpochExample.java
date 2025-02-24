cee050455fdfccaffa3bd13de947b7c3af5720c9
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2025-02-23 22:27:35 -0500

    Update DateToEpochExample.java

--- DateToEpochExample.java
+++ DateToEpochExample.java
@@ -1,9 +1,13 @@
-Subject: Frequent Issue with Ruleset Execution in Test RES
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-Dear [RES Admin's Name],
+public class Test {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        EmpowermentResponse response = new EmpowermentResponse();
+        response.setEmpowermentRestrictionDetails(new EmpowermentRestrictionDetails()); // Empty object
 
-We have observed a recurring issue in our Test Rule Execution Server (RES) where a newly deployed ruleset with the same version is not referenced until the RES instance is restarted. This has been impacting both the Business Teams and IT Teams by slowing down the validation of the decision service.
-
-To help address this, could you confirm the current value of ruleset.maxIdleTime in our Test RES? Ideally, this value should not be set to 0 and should be -1 in lower environments to ensure that newly deployed RuleApps are parsed immediately.
-
-Please let us know if any adjustments can be made to resolve this issue effectively.
+        String json = objectMapper.writeValueAsString(response);
+        System.out.println(json); // Should print "{}" (without empty object)
+    }
+}
