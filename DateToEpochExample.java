16b289ca7f2b0c486d4c38430357cf6e5c80494d
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2025-05-14 14:06:29 -0400

    Update DateToEpochExample.java

--- DateToEpochExample.java
+++ DateToEpochExample.java
@@ -1,5 +1,7 @@
-Thanks for bringing this up.
+Subject: Teams Testing Older Version – Please Validate Against Latest v1.1
 
-The version you're currently testing appears to be outdated. The change to include 4 whitespaces for statementFields was introduced as part of an updated requirement and has been incorporated in version 1.1, which is now available in the test environment.
+Hi [Lead's Name],
 
-Please make sure you’re testing against v1.1 to validate the latest behavior.
+I wanted to bring to your attention that some of the teams are currently testing against an older version of the service. A few individuals have reached out to me regarding discrepancies that are actually addressed in version 1.1, which is already deployed and available in the test environment.
+
+Could you please help communicate to the respective testing teams to ensure they validate against the latest v1.1 version?
